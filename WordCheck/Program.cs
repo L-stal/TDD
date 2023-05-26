20f5064dddd4c9b537bf295072be5e39789dc9c7
L-stal <stalenhag.leo@gmail.com>
2023-05-26 10:43:50 +0200

    Workdchecker and length

---
+++ WordCheck/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace WordCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class WordChecker
+    {
+        public string LengthChecker(string input)//Check the lenght
+        {
+            int numLetters = input.Length;
+
+            return numLetters.ToString();
+        }
+        public string CheckPalin(string input)//Checks if the word i a Palndrome word , mening you spell is the same forward and backwards
+        {
+            string inputstr, reverse = string.Empty;
+            inputstr = input;
+
+
+            for (int i = inputstr.Length - 1; i >= 0; i--)
+            {
+                reverse += inputstr[i].ToString();
+            }
+            return reverse;
+        }
+    }
+}
